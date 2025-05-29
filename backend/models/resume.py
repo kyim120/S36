2cbd1f119b6e7917a5b3263d0543f388d3ff0eea
Kyim <kyim@Kyim.Kyim>
2025-05-29 10:16:37 -0700

    Remove backend code from master branch

--- backend/models/resume.py
+++
@@ -1,6 +0,0 @@
-class Resume:
-    def __init__(self, user, education, experience, skills):
-        self.user = user
-        self.education = education
-        self.experience = experience
-        self.skills = skills
